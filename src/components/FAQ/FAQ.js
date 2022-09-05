c036b7db357c0fe7b853d9e1770c283d98f8bd7a
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-09-05 20:37:16 +0800

    Create FAQs section

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -2,6 +2,49 @@ import './FAQ.css'
 
 export default function FAQ() {
     return (
-        <section id="amac-faq"></section>
+        <section id="amac-faq">
+            <div className="container">
+                <p className="text-center font-size-lg-280 text-white arvo-bold faq-title mb-4">FREQUENTLY ASKED QUESTIONS</p>
+
+                <div className="accordion accordion-flush" id="faq-accordion">
+                    <div className="accordion-item">
+                        <h2 className="accordion-header" id="flush-header-1">
+                            <button className="accordion-button text-center justify-content-center collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse-1">
+                                Question #1
+                            </button>
+                        </h2>
+                        <div id="flush-collapse-1" className="accordion-collapse collapse" data-bs-parent="#faq-accordion">
+                            <div className="accordion-body">
+                                <p className="font-size-lg-120">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="accordion-item">
+                        <h2 className="accordion-header" id="flush-header-2">
+                            <button className="accordion-button text-center justify-content-center collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse-2">
+                                Question #2
+                            </button>
+                        </h2>
+                        <div id="flush-collapse-2" className="accordion-collapse collapse" data-bs-parent="#faq-accordion">
+                            <div className="accordion-body">
+                                <p className="font-size-lg-120">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="accordion-item">
+                        <h2 className="accordion-header" id="flush-header-1">
+                            <button className="accordion-button text-center justify-content-center collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse-3">
+                                Question #3
+                            </button>
+                        </h2>
+                        <div id="flush-collapse-3" className="accordion-collapse collapse" data-bs-parent="#faq-accordion">
+                            <div className="accordion-body">
+                                <p className="font-size-lg-120">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,15 @@
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import './Footer.css'
 
 export default function Footer() {
     return (
-        <footer id="amac-footer"></footer>
+        <footer id="amac-footer" className="bg-color-2">
+            <div className="container">
+                <div className="footer-icons d-flex justify-content-center align-items-center">
+                    
+                </div>
+            </div>
+        </footer>
     )
 }
