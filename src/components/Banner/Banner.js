17e0ed4393da5349d106aa39c8841f7c9de0e19c
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-09-09 09:07:22 +0800

    Update banner section

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -11,9 +11,10 @@ export default function Banner({ showRegister }) {
                     <div className="amac-logo">
                         <img className="w-100" src={logo} alt="AMAC Logo" />
                     </div>
-                    <p className="font-size-130 font-size-sm-160 font-size-md-200 font-size-lg-280 arvo-bold text-center text-color-1 banner-title">ALBAY MULTIMEDIA ARTS CONVENTION</p>
-                    <p className="font-size-100 font-size-sm-120 font-size-md-150 font-size-lg-230 arvo-bold text-center text-white mb-5 banner-sub">October 8-9, 2022 | Ayala Malls Legazpi</p>
-                    <button onClick={showRegister} className="def btn btn-custom-1 px-5 banner-btn">BUY TICKETS</button>
+                    <p className="font-size-130 font-size-sm-160 font-size-md-200 font-size-lg-280 arvo-bold text-center text-color-1 banner-title">Exploring the intersection of art and technology</p>
+                    <p className="font-size-100 font-size-sm-120 font-size-md-150 font-size-lg-180 arvo-bold text-center text-white mb-5 banner-sub">October 8-9, 2022 | Ayala Malls Legazpi</p>
+                    <button onClick={showRegister} className="def btn btn-custom-1 px-5 mb-3 banner-btn">BUY TICKETS</button>
+                    <p className="font-size-80 font-size-md-100 arvo-italic text-center text-white mb-0 banner-text">Note: Only the Art Talks is ticketed, all activities is open and free for the community</p>
                 </div>
             </div>
         </section>
