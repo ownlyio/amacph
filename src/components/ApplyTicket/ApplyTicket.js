c8941d6ebc18f5b98e4db15ffafc3de9cac9ba76
Bernard Historillo <bernard.historillo@bicol-u.edu.ph>
2022-09-10 16:45:54 +0800

    Update layout of ticket inclusions section

--- src/components/ApplyTicket/ApplyTicket.js
+++ src/components/ApplyTicket/ApplyTicket.js
@@ -10,16 +10,19 @@ export default function ApplyTicket({ state, showRegister }) {
         <section id="tickets">
             <div className="container">
                 <div className="row align-items-center">
-                    <div className="col-12 col-md-5 mb-5 mb-md-0">
-                        <p className="text-center text-md-start font-size-130 font-size-sm-160 font-size-md-200 font-size-lg-280 text-white arvo-bold ticket-title">BUY TICKETS</p>
+                    <div className="col-12 mb-5 mb-md-0">
+                        <p className="text-center w-100 font-size-130 font-size-sm-160 font-size-md-200 font-size-lg-280 text-white arvo-bold ticket-title mb-1">BUY TICKETS</p>
+                        <p className="text-center w-100 font-size-90 font-size-md-100 font-size-lg-140 text-white arvo-bold ticket-title mb-5">TICKET INCLUSIONS:</p>
+                    </div>
 
+                    <div className="col-12 col-md-6 pe-md-4">
                         <div className="row justify-content-center align-items-center mb-4 mb-lg-0">
                             <div className="col-12 col-md-5">
                                 <div className="ticket-img-wrap">
                                     <div className="ticket-img mx-auto">
                                         <img src={shirt} alt="AMAC Shirt" className="w-100" />
                                     </div>
-                                    <p className="text-center font-size-100 font-size-md-110 font-size-lg-130 text-white arvo-italic ticket-det mb-3 mb-lg-5">AMAC2022 X Lei Melendres Shirt</p>
+                                    <p className="text-center font-size-100 font-size-md-110 font-size-lg-130 text-white arvo-italic ticket-det mb-3">AMAC2022 X Lei Melendres Shirt</p>
                                 </div>
                             </div>
                             <div className="col-12 col-md-2">
@@ -30,13 +33,47 @@ export default function ApplyTicket({ state, showRegister }) {
                                     <div className="ticket-img mx-auto">
                                         <img src={rascal} alt="Mustachio Rascals" className="w-100" />
                                     </div>
-                                    <p className="text-center font-size-100 font-size-md-110 font-size-lg-130 text-white arvo-italic ticket-det mb-3 mb-lg-5">3D Mustachio Rascals</p>
+                                    <p className="text-center font-size-100 font-size-md-110 font-size-lg-130 text-white arvo-italic ticket-det mb-3">3D Mustachio Rascals</p>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div className="col-12 col-md-6 ps-md-4">
+                        <div className="row justify-content-evenly align-items-start flex-wrap mb-md-3 mb-lg-0 font-size-80">
+                            <div className="ticket-inclusion-item p-3 mb-2">
+                                <div className="text-center mb-3">
+                                    <FontAwesomeIcon className="inclusion-icon" icon={faTicketAlt} color="white" size="4x" />
+                                </div>
+                                <p className="text-center font-size-100 font-size-sm-110 font-size-lg-140 text-white inclusion-desc mb-2">Event Admission</p>
+                                <p className="text-center font-size-80 font-size-sm-90 font-size-lg-110 text-white arvo-italic inclusion-desc-sub mb-0">4 speakers sharing their art journey filled-with inspiration and learnings</p>
+                            </div>
+                            <div className="ticket-inclusion-item p-3 mb-2">
+                                <div className="text-center mb-3">
+                                    <FontAwesomeIcon className="inclusion-icon" icon={faGifts} color="white" size="4x" />
+                                </div>
+                                <p className="text-center font-size-100 font-size-sm-110 font-size-lg-140 text-white inclusion-desc mb-2">AMAC2022 freebies</p>
+                                <p className="text-center font-size-80 font-size-sm-90 font-size-lg-110 text-white arvo-italic inclusion-desc-sub mb-0">Event shirt designed by Lei Melendres and more!</p>
+                            </div>
+                            <div className="ticket-inclusion-item p-3">
+                                <div className="text-center mb-3">
+                                    <FontAwesomeIcon className="inclusion-icon" icon={faUsers} color="white" size="4x" />
+                                </div>
+                                <p className="text-center font-size-100 font-size-sm-110 font-size-lg-140 text-white inclusion-desc mb-2">Networking</p>
+                                <p className="text-center font-size-80 font-size-sm-90 font-size-lg-110 text-white arvo-italic inclusion-desc-sub mb-0">Connect and meet fellow creative minds in the event</p>
+                            </div>
+                            <div className="ticket-inclusion-item p-3">
+                                <div className="text-center mb-3">
+                                    <FontAwesomeIcon className="inclusion-icon" icon={faCertificate} color="white" size="4x" />
+                                </div>
+                                <p className="text-center font-size-100 font-size-sm-110 font-size-lg-140 text-white inclusion-desc mb-2">NFT Certificate</p>
+                                <p className="text-center font-size-80 font-size-sm-90 font-size-lg-110 text-white arvo-italic inclusion-desc-sub mb-0">Irrefutable POAP</p>
+                            </div>
+                        </div>
+                    </div>
 
+                    <div className="col-12 mt-5">
                         <div className="ticket-wrap">
-                            <p className="text-center font-size-200 font-size-sm-270 font-size-md-310 font-size-lg-400 text-white arvo-bold ticket-price mb-2">₱ 599</p>
+                            <p className="text-center font-size-270 font-size-md-310 font-size-lg-400 text-white arvo-bold ticket-price mb-2">₱599</p>
                             <p className="text-center font-size-150 font-size-sm-170 font-size-md-210 font-size-lg-300 text-color-4 arvo-bold ticket-price mb-2">ART TALK PASS</p>
                             <p className="text-center font-size-100 font-size-md-110 font-size-lg-120 text-white ticket-det mb-5">Each ticket includes an AMAC2022 X Lei Melendres shirt plus a FREE MINT slot of Mustachio RASCALS NFT</p>
 
@@ -51,40 +88,6 @@ export default function ApplyTicket({ state, showRegister }) {
 
                             <div className="text-center">
                                 <button onClick={showRegister} className="def btn btn-custom-4 px-5 ticket-btn" disabled={state.isSoldOut}>{state.isSoldOut ? "SOLD OUT" : "BUY NOW!"}</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-12 col-md-6 offset-md-1">
-                        <p className="text-center text-md-start font-size-110 font-size-sm-130 font-size-md-180 font-size-lg-230 text-white arvo-bold ticket-sub mb-5">INCLUSIONS</p>
-
-                        <div className="row justify-content-evenly align-items-start flex-wrap mb-md-3 mb-lg-0">
-                            <div className="ticket-inclusion-item p-3 mb-4">
-                                <div className="text-center mb-4">
-                                    <FontAwesomeIcon className="inclusion-icon" icon={faTicketAlt} color="white" size="4x" />
-                                </div>
-                                <p className="text-center font-size-100 font-size-sm-110 font-size-lg-140 text-white inclusion-desc mb-2">Event Admission</p>
-                                <p className="text-center font-size-80 font-size-sm-90 font-size-lg-110 text-white arvo-italic inclusion-desc-sub mb-0">4 speakers sharing their art journey filled-with inspiration and learnings</p>
-                            </div>
-                            <div className="ticket-inclusion-item p-3 mb-4">
-                                <div className="text-center mb-4">
-                                    <FontAwesomeIcon className="inclusion-icon" icon={faGifts} color="white" size="4x" />
-                                </div>
-                                <p className="text-center font-size-100 font-size-sm-110 font-size-lg-140 text-white inclusion-desc mb-2">AMAC2022 freebies</p>
-                                <p className="text-center font-size-80 font-size-sm-90 font-size-lg-110 text-white arvo-italic inclusion-desc-sub mb-0">Event shirt designed by Lei Melendres and more!</p>
-                            </div>
-                            <div className="ticket-inclusion-item p-3">
-                                <div className="text-center mb-4">
-                                    <FontAwesomeIcon className="inclusion-icon" icon={faUsers} color="white" size="4x" />
-                                </div>
-                                <p className="text-center font-size-100 font-size-sm-110 font-size-lg-140 text-white inclusion-desc mb-2">Networking</p>
-                                <p className="text-center font-size-80 font-size-sm-90 font-size-lg-110 text-white arvo-italic inclusion-desc-sub mb-0">Connect and meet fellow creative minds in the event</p>
-                            </div>
-                            <div className="ticket-inclusion-item p-3">
-                                <div className="text-center mb-4">
-                                    <FontAwesomeIcon className="inclusion-icon" icon={faCertificate} color="white" size="4x" />
-                                </div>
-                                <p className="text-center font-size-100 font-size-sm-110 font-size-lg-140 text-white inclusion-desc mb-2">NFT Certificate</p>
-                                <p className="text-center font-size-80 font-size-sm-90 font-size-lg-110 text-white arvo-italic inclusion-desc-sub mb-0">Irrefutable POAP</p>
                             </div>
                         </div>
                     </div>
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,11 +5,13 @@ import logo from '../../img/amac-logo-2.png'
 
 export default function Banner({ showRegister }) {
     return (
-        <section id="amac-banner" className="h-screen background-image-cover position-relative" style={{"backgroundImage": `url(${banner})`}}>
-            <div className="container h-100">
+        <section id="amac-banner" className="h-screen background-image-cover position-relative py-5" style={{"backgroundImage": `url(${banner})`}}>
+            <div className="container h-100 pt-5">
                 <div className="d-flex justify-content-center align-items-center h-100 flex-column">
-                    <div className="amac-logo">
-                        <img className="w-100" src={logo} alt="AMAC Logo" />
+                    <div className="row justify-content-center mb-3">
+                        <div className="col-md-8 col-lg-6 col-xl-5 col-xxl-6">
+                            <img className="w-100" src={logo} alt="AMAC Logo" />
+                        </div>
                     </div>
                     <p className="font-size-130 font-size-sm-160 font-size-md-200 font-size-lg-280 arvo-bold text-center text-color-1 banner-title">Exploring the intersection of art and technology</p>
                     <p className="font-size-100 font-size-sm-120 font-size-md-150 font-size-lg-180 arvo-bold text-center text-white mb-5 banner-sub">October 8-9, 2022 | Ayala Malls Legazpi</p>
