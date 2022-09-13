2ccf9819c61a772f4781d2d14d39ea596fc0000a
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-09-13 17:56:23 +0800

    Add community partners in sponsors

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -3,6 +3,8 @@ import './Sponsors.css'
 import coPresenter1 from '../../img/sponsors/ownly.png'
 import coPresenter2 from '../../img/sponsors/ayala-malls-legazpi.webp'
 import mediaPartner from '../../img/sponsors/bitpinas.png'
+import commPartner1 from '../../img/sponsors/crypto-art-ph.png'
+import commPartner2 from '../../img/sponsors/tpt-black.png'
 
 export default function Sponsors() {
     return (
@@ -33,6 +35,21 @@ export default function Sponsors() {
                             <div className="sponsors-row d-flex justify-content-evenly align-items-center my-md-3 my-lg-5 flex-wrap">
                                 <div className="sponsor-item my-3 my-md-0 mx-md-3">
                                     <img src={mediaPartner} alt="Sponsor Logo" className="w-100" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Community Partner */}
+                    <div className="sponsors-section">
+                        <p className="text-center font-size-100 font-size-lg-140 text-color-5 arvo-italic sponsor-title mb-sm-4 mb-md-1">Community Partners</p>
+                        <div className="sponsors-wrap">
+                            <div className="sponsors-row d-flex justify-content-evenly align-items-center my-md-3 my-lg-5 flex-wrap">
+                                <div className="sponsor-item my-3 my-md-0 mx-md-3">
+                                    <img src={commPartner1} alt="Sponsor Logo" className="w-100" />
+                                </div>
+                                <div className="sponsor-item my-3 my-md-0 mx-md-3">
+                                    <img src={commPartner2} alt="Sponsor Logo" className="w-100" />
                                 </div>
                             </div>
                         </div>
