d0a54b2ff92eb6a49cd53998d4ac66989c9265f2
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-09-15 11:43:56 +0800

    Update layout of partners and add new logos

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -5,6 +5,8 @@ import coPresenter2 from '../../img/sponsors/ayala-malls-legazpi.webp'
 import mediaPartner from '../../img/sponsors/bitpinas.png'
 import commPartner1 from '../../img/sponsors/crypto-art-ph.png'
 import commPartner2 from '../../img/sponsors/tpt-black.png'
+import commPartner3 from '../../img/sponsors/cotm.png'
+import commPartner4 from '../../img/sponsors/dawani.png'
 
 export default function Sponsors() {
     return (
@@ -13,6 +15,44 @@ export default function Sponsors() {
                 <div className="sponsors-lists">
                     <p className="text-center font-size-130 font-size-sm-160 font-size-md-200 font-size-lg-280 text-color-4 arvo-bold sponsor-title mb-5">OUR PARTNERS</p>
                     
+                    <div className="row align-items-start">
+                        <div className="col-12 col-md-6">
+                            {/* Community Partner */}
+                            <div className="sponsors-section">
+                                <p className="text-center font-size-100 font-size-lg-140 text-color-5 arvo-italic sponsor-title mb-sm-4 mb-md-1">Community Partners</p>
+                                <div className="sponsors-wrap">
+                                    <div className="sponsors-row d-flex justify-content-evenly align-items-center my-md-3 my-lg-5 flex-wrap">
+                                        <div className="sponsor-item-sm my-3 my-md-0 mx-lg-4 mx-xxl-5">
+                                            <img src={commPartner1} alt="Sponsor Logo" className="w-100" />
+                                        </div>
+                                        <div className="sponsor-item-sm my-3 my-md-0 mx-lg-4 mx-xxl-5">
+                                            <img src={commPartner2} alt="Sponsor Logo" className="w-100" />
+                                        </div>
+                                        <div className="sponsor-item-sm my-3 my-md-0 mx-lg-4 mx-xxl-5">
+                                            <img src={commPartner3} alt="Sponsor Logo" className="w-100" />
+                                        </div>
+                                        <div className="sponsor-item-sm my-3 my-md-0 mx-lg-4 mx-xxl-5">
+                                            <img src={commPartner4} alt="Sponsor Logo" className="w-100" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                            {/* Official Media Partner */}
+                            <div className="sponsors-section">
+                                <p className="text-center font-size-100 font-size-lg-140 text-color-5 arvo-italic sponsor-title mb-sm-4 mb-md-1">Official Media Partner</p>
+                                <div className="sponsors-wrap">
+                                    <div className="sponsors-row d-flex justify-content-evenly align-items-center my-md-3 my-lg-5 flex-wrap">
+                                        <div className="sponsor-item my-3 my-md-0 mx-md-3">
+                                            <img src={mediaPartner} alt="Sponsor Logo" className="w-100" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>  
+                        </div>                                              
+                    </div>
+
                     {/* Co-Presenters */}
                     <div className="sponsors-section">
                         <p className="text-center font-size-100 font-size-lg-140 text-color-5 arvo-italic sponsor-title mb-sm-4 mb-md-1">Organized and Co-presented by</p>
@@ -23,33 +63,6 @@ export default function Sponsors() {
                                 </div>
                                 <div className="sponsor-item my-3 my-md-0 mx-md-3">
                                     <img src={coPresenter2} alt="Sponsor Logo" className="w-100" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Official Media Partner */}
-                    <div className="sponsors-section">
-                        <p className="text-center font-size-100 font-size-lg-140 text-color-5 arvo-italic sponsor-title mb-sm-4 mb-md-1">Official Media Partner</p>
-                        <div className="sponsors-wrap">
-                            <div className="sponsors-row d-flex justify-content-evenly align-items-center my-md-3 my-lg-5 flex-wrap">
-                                <div className="sponsor-item my-3 my-md-0 mx-md-3">
-                                    <img src={mediaPartner} alt="Sponsor Logo" className="w-100" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Community Partner */}
-                    <div className="sponsors-section">
-                        <p className="text-center font-size-100 font-size-lg-140 text-color-5 arvo-italic sponsor-title mb-sm-4 mb-md-1">Community Partners</p>
-                        <div className="sponsors-wrap">
-                            <div className="sponsors-row d-flex justify-content-evenly align-items-center my-md-3 my-lg-5 flex-wrap">
-                                <div className="sponsor-item my-3 my-md-0 mx-md-3">
-                                    <img src={commPartner1} alt="Sponsor Logo" className="w-100" />
-                                </div>
-                                <div className="sponsor-item my-3 my-md-0 mx-md-3">
-                                    <img src={commPartner2} alt="Sponsor Logo" className="w-100" />
                                 </div>
                             </div>
                         </div>
