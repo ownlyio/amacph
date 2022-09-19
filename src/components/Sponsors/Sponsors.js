7530328690112ec6f9ec6395c5f0c3f2372c7438
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-09-19 17:40:49 +0800

    Add SparkPoint Logo in partners

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -9,6 +9,7 @@ import commPartner2 from '../../img/sponsors/tpt-black.png'
 import commPartner3 from '../../img/sponsors/cotm.png'
 import commPartner4 from '../../img/sponsors/dawani.png'
 import goldSponsor1 from '../../img/sponsors/coinex.png'
+import goldSponsor2 from '../../img/sponsors/sparkpoint.png'
 import bronzeSponsor1 from '../../img/sponsors/placewar.png'
 
 export default function Sponsors() {
@@ -18,30 +19,28 @@ export default function Sponsors() {
                 <div className="sponsors-lists">
                     <p className="text-center font-size-130 font-size-sm-160 font-size-md-200 font-size-lg-280 text-color-4 arvo-bold sponsor-title mb-5">OUR PARTNERS</p>
 
-                    <div className="row align-items-start">
-                        <div className="col-12 col-md-6">
-                            {/* Gold Sponsors */}
-                            <div className="sponsors-section">
-                                <p className="text-center font-size-100 font-size-lg-140 text-color-5 arvo-italic sponsor-title mb-sm-4 mb-md-1">Gold Sponsor</p>
-                                <div className="sponsors-wrap">
-                                    <div className="sponsors-row d-flex justify-content-evenly align-items-center my-md-3 my-lg-5 flex-wrap">
-                                        <div className="sponsor-item my-3 my-md-0 mx-lg-4 mx-xxl-5">
-                                            <img src={goldSponsor1} alt="Sponsor Logo" className="w-100" />
-                                        </div>
-                                    </div>
+                    {/* Gold Sponsors */}
+                    <div className="sponsors-section">
+                        <p className="text-center font-size-100 font-size-lg-140 text-color-5 arvo-italic sponsor-title mb-sm-4 mb-md-1">Gold Sponsors</p>
+                        <div className="sponsors-wrap">
+                            <div className="sponsors-row d-flex justify-content-evenly align-items-center my-md-3 my-lg-5 flex-wrap">
+                                <div className="sponsor-item gold my-3 my-md-0 mx-md-3">
+                                    <img src={goldSponsor1} alt="Sponsor Logo" className="w-100" />
+                                </div>
+                                <div className="sponsor-item gold my-3 my-md-0 mx-md-3">
+                                    <img src={goldSponsor2} alt="Sponsor Logo" className="w-100" />
                                 </div>
                             </div>
                         </div>
-                        <div className="col-12 col-md-6">
-                            {/* Bronze Sponsors */}
-                            <div className="sponsors-section">
-                                <p className="text-center font-size-100 font-size-lg-140 text-color-5 arvo-italic sponsor-title mb-sm-4 mb-md-1">Bronze Sponsor</p>
-                                <div className="sponsors-wrap">
-                                    <div className="sponsors-row d-flex justify-content-evenly align-items-center my-md-3 my-lg-5 flex-wrap">
-                                        <div className="sponsor-item my-3 my-md-0 mx-md-3">
-                                            <img src={bronzeSponsor1} alt="Sponsor Logo" className="w-100" />
-                                        </div>
-                                    </div>
+                    </div>
+
+                    {/* Bronze Sponsors */}
+                    <div className="sponsors-section">
+                        <p className="text-center font-size-100 font-size-lg-140 text-color-5 arvo-italic sponsor-title mb-sm-4 mb-md-1">Bronze Sponsor</p>
+                        <div className="sponsors-wrap">
+                            <div className="sponsors-row d-flex justify-content-evenly align-items-center my-md-3 my-lg-5 flex-wrap">
+                                <div className="sponsor-item my-3 my-md-0 mx-md-3">
+                                    <img src={bronzeSponsor1} alt="Sponsor Logo" className="w-100" />
                                 </div>
                             </div>
                         </div>
@@ -73,7 +72,7 @@ export default function Sponsors() {
                         <div className="col-12 col-md-6">
                             {/* Official Media Partner */}
                             <div className="sponsors-section">
-                                <p className="text-center font-size-100 font-size-lg-140 text-color-5 arvo-italic sponsor-title mb-sm-4 mb-md-1">Official Media Partner</p>
+                                <p className="text-center font-size-100 font-size-lg-140 text-color-5 arvo-italic sponsor-title mb-sm-4 mb-md-1">Official Media Partners</p>
                                 <div className="sponsors-wrap">
                                     <div className="sponsors-row d-flex justify-content-evenly align-items-center my-md-3 my-lg-5 flex-wrap">
                                         <div className="sponsor-item-md my-3 my-md-0 mx-md-3">
